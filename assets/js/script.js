2dee3d2df2229c6cc57098ee3d39881c67cc111f
Marino <marinoco.has@gmail.com>
2024-04-26 20:54:45 +0900

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,49 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// svg読み込み
+document.addEventListener("DOMContentLoaded", function () {
+    const svgElements = document.querySelectorAll("[data-svg]");
+
+    svgElements.forEach(function (element) {
+        const svgFilePath = element.getAttribute("data-svg");
+        const svgUrl = `../assets/images/${svgFilePath}.svg`;
+
+        fetch(svgUrl)
+            .then((response) => response.text())
+            .then((svgContent) => {
+                element.innerHTML = svgContent;
+                element.removeAttribute("data-svg");
+            })
+            .catch((error) => {
+                console.error("SVGの読み込みエラー:", error);
+            });
+    });
+});
+
+// サブコンテンツの移動
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarItem = document.querySelector(".aside-section");
+    const main = document.querySelector(".main");
+
+    let isSmallScreen = window.innerWidth <= 768;
+
+    function handleResize() {
+        const currentIsSmallScreen = window.innerWidth <= 768;
+
+        if (currentIsSmallScreen !== isSmallScreen) {
+            isSmallScreen = currentIsSmallScreen;
+
+            if (isSmallScreen) {
+                main.appendChild(sidebarItem);
+            } else {
+                sidebar.appendChild(sidebarItem);
+            }
+        }
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+});
